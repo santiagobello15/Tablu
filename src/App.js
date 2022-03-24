7ca3980e053806dcc13e26c2fa64b7d2c1160377
santiagobello15 <santiagobello15@gmail.com>
2022-03-24 20:45:38 -0300

    styled

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="app-header">
+        <h1 className="header-title">TABLÚ FAMOSOS</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
