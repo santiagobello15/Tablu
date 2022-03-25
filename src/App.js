2c62dc4360e03f27a77a3836e92a4fdbe029b2a7
santiagobello15 <santiagobello15@gmail.com>
2022-03-25 20:28:50 -0300

    added buttons, styled, etc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import "./App.css";
+import iphone from "./media/iphone-front-transparent.png";
+import iosBadge from "./media/badge-ios.svg";
+import androidBadge from "./media/badge-android.svg";
 
 function App() {
   return (
     <div className="App">
       <div className="app-header">
         <h1 className="header-title">TABLÚ FAMOSOS</h1>
+      </div>
+      <div className="body-div">
+        <div className="iphone-container">
+          <img className="iphone-pic" src={iphone}></img>
+          <div className="body-text-container">
+            <h2 className="body-text body-text-title">JUGÁ TABLÚ FAMOSOS</h2>
+            <h2 className="body-text body-text-subt">Divertite con amigos</h2>
+            <p className="body-text body-text-download">
+              Descargá la app en tu smartphone
+            </p>
+            <div className="buttons-cointainer">
+              <img className="button-app" src={iosBadge}></img>
+            </div>
+            <div className="buttons-cointainer">
+              <img className="button-app app-android" src={androidBadge}></img>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
