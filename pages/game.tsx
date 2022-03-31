761746928d2e72509176b7674a56f4a30b13b936
santiagobello15 <santiagobello15@gmail.com>
2022-03-31 00:30:53 -0300

    continued to style plus added game page

---
+++ pages/game.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import styles from "../styles/game.module.scss";
+const Game: NextPage = () => {
+  return (
+    <div className={styles.mainDiv}>
+      <h1>Start</h1>
+    </div>
+  );
+};
+export default Game;
