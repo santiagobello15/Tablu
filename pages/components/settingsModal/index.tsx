9326f1f640f134a7053f82de1f8a0c33c8b0a6fa
santiagobello15 <santiagobello15@gmail.com>
2022-04-01 00:42:12 -0300

    styled, added setting.

--- pages/components/settingsModal/index.tsx
+++ pages/components/settingsModal/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styles from "./settingsModal.module.scss";
 
-function settingsModal({ closeSettingsModal }) {
+function settingsModal({ closeSettingsModal }: any) {
   const closeSettModal = () => {
     closeSettingsModal(false);
   };
@@ -10,7 +10,22 @@ function settingsModal({ closeSettingsModal }) {
     <div className={styles.overlayModal}>
       <div className={styles.modalContainer}>
         <h1>Configuración</h1>
-
+        <ul className={styles.configUl}>
+          <li className={styles.firstLi}>
+            <p>Cantidad de rondas</p>
+          </li>
+          <li className={styles.secondLi}>
+            <p>Tiempo por ronda</p>
+          </li>
+          <li className={styles.thirdLi}>
+            <p>Tiempo por ronda</p>
+          </li>
+          <li className={styles.fourthLi}></li>
+          <li className={styles.fifthLi}>
+            <p>Incrementos de dificultad</p>
+          </li>
+        </ul>
+        {}
         <div className={styles.closeBtn} onClick={closeSettModal}>
           <p>X</p>
         </div>
