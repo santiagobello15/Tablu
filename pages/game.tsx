2e8e24a584f0633257793acda5ad10b3defbd468
santiagobello15 <santiagobello15@gmail.com>
2022-04-03 21:12:00 -0300

    styled, added multiple btns. points, pass

--- pages/game.tsx
+++ pages/game.tsx
@@ -16,10 +16,14 @@ const Game: NextPage = () => {
   const [teamOneColor, setTeamOneColor] = useState("Red");
   const [teamTwoColor, setTeamTwoColor] = useState("Blue");
   const [teamOneName, setTeamOneName] = useState("Team 1");
-  const [teamTwoName, setTeamTwoName] = useState("Team 1");
+  const [teamTwoName, setTeamTwoName] = useState("Team 2");
   const [centerDivOpacity, setCenterDivOpacity] = useState(1);
-  const [countDownGame, setCountDownGame] = useState(timeRound);
+  const [countDownGame, setCountDownGame] = useState(60);
   const [startCounter, setStartCounter] = useState(false);
+  const [currentRonda, setCurrentRonda] = useState(1);
+  const [activeTeamOne, setActiveTeamOne] = useState(true);
+  const [pointsTeamOne, setPointsTeamOne] = useState(0);
+  const [pointsTeamTwo, setPointsTeamTwo] = useState(0);
 
   const rendersettingsModal = () => {
     if (showsettingsModal == true) {
@@ -61,6 +65,7 @@ const Game: NextPage = () => {
   };
   const startModalShow = () => {
     setShowStartModal(true);
+    setCountDownGame(timeRound);
   };
 
   const muletillaFunction = () => {
@@ -119,13 +124,6 @@ const Game: NextPage = () => {
     }
   };
 
-  /*   const CounterFunction = () => {
-    for (let i = timeRound; i > 0; i--) {
-      console.log(i);
-      setTimeout(CounterFunction(), 1000);
-    }
-  };
- */
   const TeamOneCardColor = () => {
     if (teamOneColor == "Red") {
       return styles.cardRedTeamOne;
@@ -154,6 +152,42 @@ const Game: NextPage = () => {
   function CounterFunction() {
     setStartCounter(true);
   }
+  function PauseCounterFunction() {
+    setStartCounter(false);
+  }
+
+  const RestartCounter = () => {
+    if (startCounter == true) {
+      alert("primero pausá");
+    } else {
+      setCountDownGame(timeRound);
+    }
+  };
+
+  const PauseBtn = () => {
+    if (startCounter == true) {
+      return (
+        <div className={styles.pauseBtn} onClick={PauseCounterFunction}>
+          <p>Pausar</p>
+        </div>
+      );
+    }
+    return (
+      <div className={styles.startRoundBtn} onClick={CounterFunction}>
+        <p>Iniciar</p>
+      </div>
+    );
+  };
+
+  const RestartBtn = () => {
+    if (startCounter == false) {
+      return (
+        <div className={styles.restartBtn} onClick={RestartCounter}>
+          <p>Reiniciar</p>
+        </div>
+      );
+    }
+  };
 
   useEffect(() => {
     if (startCounter == true) {
@@ -162,23 +196,92 @@ const Game: NextPage = () => {
     }
   });
 
+  const AddPoints = () => {
+    if (activeTeamOne == true) {
+      setPointsTeamOne(pointsTeamOne + 1);
+    } else {
+      setPointsTeamTwo(pointsTeamTwo + 1);
+    }
+  };
+  const DeductPoints = () => {
+    if (activeTeamOne == true) {
+      setPointsTeamOne(pointsTeamOne - 1);
+    } else {
+      setPointsTeamTwo(pointsTeamTwo - 1);
+    }
+  };
+
+  const CurrTeamShow = () => {
+    if (activeTeamOne == true) {
+      return teamOneName;
+    }
+    {
+      return teamTwoName;
+    }
+  };
+
+  const FinishedAlert = () => {
+    if (countDownGame == 0) {
+      setStartCounter(false);
+      setCountDownGame(timeRound);
+      setActiveTeamOne(false);
+    }
+  };
+  FinishedAlert();
+
   const PlayingDiv = () => {
     if (centerDivOpacity == 0) {
       return (
         <div className={styles.centerDiv}>
           <h1 className={styles.headerTitle}>TABLÚ FAMOSOS</h1>
-          <div className={styles.startRoundBtn} onClick={CounterFunction}>
-            <p>Iniciar</p>
-          </div>
-          <div className={styles.startRound}>
-            <p>{countDownGame}</p>
+          <div className={styles.gamingPad}>
+            <div className={styles.gameCardLeft}>
+              <div className={styles.startRound}>
+                <p>{countDownGame}</p>
+              </div>
+              <div className={styles.roundsDiv}>
+                <p>Ronda: {currentRonda}</p>
+              </div>
+            </div>
+            <div className={styles.gameCard}>
+              <h1>SUSANA</h1>
+              <h2>GIMENEZ</h2>
+              <ul className={styles.cardsUL1}>
+                <li>CARLOS</li>
+                <li>ZAMORA</li>
+                <li>BOXEO</li>
+              </ul>
+              <ul className={styles.cardsUL2}>
+                <li>CARLOS</li>
+                <li>ZAMORA</li>
+                <li>BOXEO</li>
+              </ul>
+            </div>
+            <div className={styles.gameCardRight}>
+              <div className={styles.answerOk} onClick={AddPoints}>
+                <p>+1 Punto</p>
+              </div>
+              <div className={styles.answerPass}>
+                <p>Pasar</p>
+              </div>
+              <div className={styles.answerWrong} onClick={DeductPoints}>
+                <p>-1 Punto</p>
+              </div>
+            </div>
           </div>
           <div className={styles.gameCounter}>
+            <div className={styles.roundOfTeam}>
+              <p>Turno del equipo: {CurrTeamShow()}</p>
+            </div>
             <div className={TeamOneCardColor()}>
               <p>{teamOneName}</p>
+              <a>{pointsTeamOne}</a>
             </div>
+            {PauseBtn()}
+            {RestartBtn()}
             <div className={TeamTwoCardColor()}>
               <p>{teamTwoName}</p>
+              <a>{pointsTeamTwo}</a>
             </div>
           </div>
         </div>
